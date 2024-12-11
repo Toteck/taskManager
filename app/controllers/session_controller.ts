da64e59a5352e1ae6e49781f5dfe23309c7edf39
toteck <mateusweslley14@gmail.com>
2024-12-11 16:59:50 -0300

    fix session controller

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -1,4 +1,18 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class SessionController {
+  async store({ request }: HttpContext) {
+    const { email, password } = await request.validateUsing(createSessionValidator)
+
+    const user = await User.verifyCredentials(email, password)
+
+    return User.accessTokens.create(user)
+  }
+
+  async destroy({ auth, response }: HttpContext) {
+    const user = auth.user!
+    await User.accessTokens.delete(user, user.currentAccessToken.identifier)
+    return response.status(203)
+  }
 }
