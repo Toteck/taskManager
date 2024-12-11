e236fdfdb72242e0919642a03976a2a067ff994f
toteck <mateusweslley14@gmail.com>
2024-12-11 17:23:38 -0300

    create index tasks controller

--- app/controllers/tasks_controller.ts
+++ app/controllers/tasks_controller.ts
@@ -2,4 +2,24 @@ import User from '#models/user'
 import { createSessionValidator } from '#validators/session'
 import type { HttpContext } from '@adonisjs/core/http'
 
-export default class TasksController {}
+export default class TasksController {
+  async index({ auth, response }: HttpContext) {
+    try {
+      const user = auth.user
+
+      if (!user) {
+        throw new Error('User not authenticated')
+      }
+
+      await user.preload('tasks')
+
+      return response.ok(user)
+    } catch (error) {
+      return response.status(401).json({ error: error.message })
+    }
+  }
+  async store({ request, auth }: HttpContext) {}
+  async show() {}
+  async update() {}
+  async destroy() {}
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,16 @@
 const UsersController = () => import('#controllers/users_controller')
+const SessionController = () => import('#controllers/session_controller')
+const TasksController = () => import('#controllers/tasks_controller')
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
+router.post('session', [SessionController, 'store'])
 
 router.resource('user', UsersController).apiOnly()
+
+router
+  .group(() => {
+    router.resource('task', TasksController).apiOnly()
+  })
+  .use(middleware.auth())
